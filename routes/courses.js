80a74f866a0044bc149986e45c9b119573408e1b
Shantanu SHubham <shantanu0707@gmail.com>
2020-12-30 20:23:56 +0530

    [Add Node Server]

--- routes/courses.js
+++ routes/courses.js
@@ -3,15 +3,17 @@ const router = express.Router();
 const demoClassRequestController = require("../controllers/demoClassRequestsController.js");
 const registrationController = require("../controllers/registrationController");
 
-router.get("/course", (req, res) => {
+router.get("/courses", (req, res) => {
   res.render("allCourses");
 });
 
-router.get("/courses-web", (req, res) => {
-  res.render("course-web");
+router.get("/courses/:name", (req, res) => {
+  let name = req.params.name;
+  if (name === "ai") {
+    res.render("course-ai");
+  }
 });
-
-router.post("/request-demo/:courseId", demoClassRequestController.createDemoClassRequest);
-router.post("/register/:courseId", registrationController.createRegistration);
+// router.post("/request-demo/:courseId", demoClassRequestController.createDemoClassRequest);
+// router.post("/register/:courseId", registrationController.createRegistration);
 
 module.exports = router;
